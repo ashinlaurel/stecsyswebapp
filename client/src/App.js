1d03cd9b68d13291eebb947ecf2ea7b57a70d574
ashinlaurel <ashinlaurel1999@gmail.com>
2020-06-12 00:31:27 +0530

    clean React

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import "./App.css";
-import "./css/styles.css"
+import "./css/styles.css";
 import axios from "axios";
 import Home from "./Components/Home";
 import { BrowserRouter, Route } from "react-router-dom";
@@ -10,22 +9,22 @@ import Order from "./Components/Order";
 // axios.defaults.baseURL =
 //   "https://us-central1-stecsys-app.cloudfunctions.net/api";
 // --------Also dont forget to change proxy in client's package.json---------------------------------------------
+
 function App() {
   return (
     <div className="">
       <BrowserRouter>
-
-        <Route exact path="/"> <Home/> </Route>
-        <Route exact path="/order"> <Order/> </Route>
+        <Route exact path="/">
+          {" "}
+          <Home />{" "}
+        </Route>
+        <Route exact path="/order">
+          {" "}
+          <Order />{" "}
+        </Route>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
