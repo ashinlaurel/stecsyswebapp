905c0dcac5dc7630d89a2cf4e16f50d3803f9047
ashinlaurel <ashinlaurel1999@gmail.com>
2020-06-11 22:44:15 +0530

    added devserver.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,10 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
-axios.defaults.baseURL =
-  "https://us-central1-stecsys-app.cloudfunctions.net/api";
+// Uncomment During Production-----------------------------------------------------------------------------------
+// axios.defaults.baseURL =
+//   "https://us-central1-stecsys-app.cloudfunctions.net/api";
+// --------Also dont forget to change proxy in client's package.json---------------------------------------------
 function App() {
   return (
     <div className="App">
