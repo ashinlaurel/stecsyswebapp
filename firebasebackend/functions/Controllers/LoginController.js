47a8745c5360cf427f1614b22fcf209dfa6d5484
alantomjose@gmail.com <f20180187@goa.bits-pilani.ac.in>
2020-06-12 22:06:57 +0530

    loginBack

--- firebasebackend/functions/Controllers/LoginController.js
+++ firebasebackend/functions/Controllers/LoginController.js
@@ -1,4 +1,5 @@
 const firebase = require("../firebase").default;
+const db = firebase.firestore();
 
 const loginController = {
   signup(req, res) {
@@ -8,18 +9,49 @@ const loginController = {
       password: req.body.password,
       handle: req.body.handle,
     };
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(newUser.email, newUser.password)
-      .then((data) => {
-        return res
-          .status(201)
-          .json({ message: `new user ${data.user.uid} Created ` });
+    let token,userId;
+    db.doc(`/Users/${newUser.handle}`)
+      .get()
+      .then(doc=>{
+        if(doc.exists){
+          return res.status(400).json({handle: 'This handle Already taken'});
+        }else{
+          return firebase
+                  .auth()
+                  .createUserWithEmailAndPassword(newUser.email, newUser.password);
+        }
+      }).then((data) => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+      }).then(token=>{
+        token=token;
+        const userCred={
+          handle:newUser.handle,
+          email: newUser.email,
+          createdAt: new Date().toISOString(),
+          userId:userId
+        }
+        return db.doc(`/Users/${newUser.handle}`).set(userCred);
+      }).then(()=>{
+        return res.status(201).json({token});
       })
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json({ message: `Error ${err.code}` });
-      });
+      .catch(err=>{
+          console.log(err);
+          return res.status(500).json({error: err.code});
+      })
+
+    // firebase
+    //   .auth()
+    //   .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    //   .then((data) => {
+    //     return res
+    //       .status(201)
+    //       .json({ message: `new user ${data.user.uid} Created ` });
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     return res.status(500).json({ message: `Error ${err.code}` });
+    //   });
   },
 };
 
