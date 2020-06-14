e69459696bf8161d70f59cf89a508188d5260b32
ashinlaurel <ashinlaurel1999@gmail.com>
2020-06-14 15:16:16 +0530

    added created at and status

--- firebasebackend/functions/Controllers/OrderFormController.js
+++ firebasebackend/functions/Controllers/OrderFormController.js
@@ -5,11 +5,13 @@ const firebase = require("../firebase").default;
 const orderFormController = {
   theorder(req, res) {
     const neworder = {
+      createdat: new Date().toDateString(),
       name: req.body.name,
       phone: req.body.phone,
       email: req.body.email,
       companyname: req.body.companyname,
       orders: req.body.orders,
+      status: "active",
     };
     console.log(neworder);
     const db = firebase.firestore();
@@ -17,7 +19,7 @@ const orderFormController = {
       .add(neworder)
       .then((ref) => {
         console.log("Added document with ID: ", ref.id);
-        // return res.status(200).json({ massage: "Docidement Added" });
+        // return res.status(200).json({ message: "Document Added" });
       })
       .catch((err) => {
         console.log(err.code);
