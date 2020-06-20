9fce5c8e14b528935f509253dad3fea49397a548
ashinlaurel <ashinlaurel1999@gmail.com>
2020-06-20 18:20:27 +0530

    minor fixes

--- client/src/Components/Aboutus.js
+++ client/src/Components/Aboutus.js
@@ -6,33 +6,76 @@ export default function AboutUs() {
       <div className="relative flex flex-col min-w-0 bg-white w-full mx-6 sm:mx-32 shadow-xl rounded-lg ">
         <div className="px-6">
           <div className="text-center mt-12">
-            <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
+            <h3 className="text-4xl font-semibold leading-normal mb-1 text-gray-800 ">
               About Us
             </h3>
-            <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
-              <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{" "}
-              Since 1999
+            <div className="text-sm leading-normal mb-2 text-gray-500 font-bold uppercase">
+              -Since 1999
             </div>
           </div>
           <div className="py-10 border-t border-gray-300 text-center">
-            <div className="flex flex-wrap justify-center">
-              <div className="w-full lg:w-9/12 px-4">
-                <p className="mb-4 text-lg leading-relaxed text-gray-800">
-                  For who thoroughly her boy estimating conviction. Removed
-                  demands expense account in outward tedious do. Particular way
-                  thoroughly unaffected projection favourable mrs can projecting
-                  own. Thirty it matter enable become admire in giving. See
-                  resolved goodness felicity shy civility domestic had but.
-                  Drawings offended yet answered jennings perceive laughing six
-                  did far.{" "}
-                </p>
-                <a
-                  href="#pablo"
-                  className="font-normal text-pink-500"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  Show more
-                </a>
+            <div className="flex flex-wrap justify-center items-center">
+              <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
+                <img
+                  alt="..."
+                  className="max-w-full rounded-lg shadow-lg"
+                  src="https://images.pexels.com/photos/2047914/pexels-photo-2047914.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                />
+              </div>
+              <div className="w-full md:w-5/12 mt-6 ml-auto mr-auto px-4">
+                <div className="md:pr-12">
+                  <div className="text-pink-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300">
+                    <i className="fa fa-rocket text-xl"></i>
+                  </div>
+                  <h3 className="text-3xl font-semibold">A growing company</h3>
+                  <p className="mt-4 text-lg leading-relaxed text-gray-600">
+                    The extension comes with three pre-built pages to help you
+                    get started fater. You can change the text and images and
+                    you're good to go.
+                  </p>
+                  <ul className="list-none mt-6">
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                            <i className="fa fa-fingerprint"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-gray-600">
+                            Carefully crafted components
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                            <i className="fab fa-html5"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-gray-600">
+                            Amazing page examples
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                            <i className="far fa-paper-plane"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-gray-600">Dynamic components</h4>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
